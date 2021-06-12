ef7c71d5743e87ec10b4e173289e53e631061a5f
Sam Myatt <smyatt@rocketmail.com>
2021-06-12 01:55:49 +0100

    Many Many Conga Based Changes

--- Conga Game/Assets/Scripts/CarMovement.cs
+++ Conga Game/Assets/Scripts/CarMovement.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour
 {
     CarController controller;
+    public GameObject GameHandler;
+
+    float timer = 0;
 
     private void Awake()
     {
         controller = GetComponent<CarController>();
+        GameHandler = GameObject.FindGameObjectWithTag("GameHandler");
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer++;
         Vector2 inputVec = Vector2.zero;
 
         inputVec.x = Input.GetAxis("Horizontal");
@@ -22,10 +27,16 @@ public class CarMovement : MonoBehaviour
         controller.SetInputVec(inputVec);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Pedestrian")
         {
+            if(timer > 360)
+			{
+                Destroy(other.gameObject);
+                timer = 0;
+                GameHandler.GetComponent<GameHandler>().SpawnFollower();
+            }
             Debug.Log("cum");
         }
     }
--- Conga Game/Assets/Scripts/GameHandler.cs
+++ Conga Game/Assets/Scripts/GameHandler.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour
 {
+
+   
+    
+
+    public GameObject Follower;
+    public GameObject Leader;
+    List<GameObject> Queue;
+
+    float test = 0;
+
+    int QueueCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Queue = new List<GameObject>();
+        Leader = GameObject.FindGameObjectWithTag("Leader");
+       
+        SpawnFollower();
+       
     }
+    
+    public void SpawnFollower()
+	{
+        if(QueueCount == 0)
+        { 
+            GameObject a = (Instantiate(Follower, new Vector3(Leader.transform.position.x, Leader.transform.position.y, 0), transform.rotation) as GameObject);
+            a.GetComponent<Follow>().Target = Leader.transform;
+            Queue.Add(a);
+            QueueCount++;
+        }
+        else
+		{
 
+            GameObject a = (Instantiate(Follower, new Vector3(Queue[QueueCount-1].transform.position.x, Queue[QueueCount - 1].transform.position.y, 0), transform.rotation) as GameObject);
+            a.GetComponent<Follow>().Target = Queue[QueueCount - 1].transform;
+            Queue.Add(a);
+
+            QueueCount++;
+        }
+        
+	}
     // Update is called once per frame
     void Update()
     {
+        test++; 
+        if(test == 360)
+		{
+            //SpawnFollower();
+            test = 0;
+        }
         
     }
 }
